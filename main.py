e7334c8cf52f3d8c1226124a53331e55b8a814dd
Shivansh07-stack <shivansh.35514803123@it.mait.ac.in>
2025-07-22 16:22:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from weather import get_weather_by_city
-from weather import get_weather_by_coordinates
 
 app = FastAPI(title="Live Weather API")
-
-@app.get("/weather/coords")
-async def fetch_weather_by_coords(lat: float, lon: float):
-    data = await get_weather_by_coordinates(lat, lon)
-    if "error" in data:
-        raise HTTPException(status_code=400, detail=data["error"])
-    return {
-        "city": data["name"],
-        "temperature": data["main"]["temp"],
-        "description": data["weather"][0]["description"],
-        "wind_speed": data["wind"]["speed"]
-    }
-
 
 @app.get("/weather/{city}")
 async def fetch_weather(city: str):
@@ -28,4 +14,3 @@ async def fetch_weather(city: str):
         "description": data["weather"][0]["description"],
         "wind_speed": data["wind"]["speed"]
     }
-
